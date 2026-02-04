b3bdfabed5f0276be142c896075f9b24e1dcc882
cainlucastapp <cainlucastapp@agentformula.com>
2026-02-04 13:00:38 -0800

    added book and coffee

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    # Constructor
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+        self.current_page = 0
+
+    # Property getter for page_count
+    @property
+    def page_count(self):
+        return self._page_count
+
+    # Property setter for page_count with validation
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+   
+    # Turn the page by one
+    def turn_page(self):
+        if self.current_page < self.page_count:
+            self.current_page += 1
+            print("Flipping the page...wow, you read fast!")
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    # Constructor
+    def __init__(self, size, price):
+        self.size = size
+        self.price = price
+
+    # Property getter for size
+    @property
+    def size(self):
+        return self._size
+
+    # Property setter for size with validation
+    @size.setter
+    def size(self, value):
+        if value not in ["Small", "Medium", "Large"]:
+            print("size must be Small, Medium, or Large")
+        else:
+            self._size = value
+    
+    # Method to add a tip
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        self.price += 1
